1732026d7e9bb82431fcad840ed7ab107925e629
behruzjon5499 <behruztoxirov909515499@mail.com>
2026-06-08 17:21:24 +0500

    fix add department

---
+++ common/migrations/db/m260608_121702_create_department_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%department}}`.
+ */
+class m260608_121702_create_department_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%department}}', [
+            'id' => $this->primaryKey(),
+            'parent_id' => $this->integer(),
+            'title' => $this->string(),
+            'title_ru' => $this->string(),
+            'uid' => $this->integer(),
+            'tin' => $this->string(20),
+            'status' => $this->integer()->defaultValue(1)->notNull(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'deleted_at' => $this->integer(),
+            'created_by' => $this->integer(),
+            'updated_by' => $this->integer(),
+            'deleted_by' => $this->integer(),
+            'is_deleted' => $this->boolean()->defaultValue(false)->notNull(),
+        ]);
+        $this->createIndex('idx_department_uid', '{{%department}}', 'uid', true);
+        $this->createIndex('idx_department_parent_id', '{{%department}}', 'parent_id', true);
+        $this->createIndex('idx_department_title', '{{%department}}', 'title', true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%department}}');
+    }
+}
